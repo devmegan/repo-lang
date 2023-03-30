91740c4e0daea91dd25e83daec7ad0234e31fca4
devmegan <devmegan@protonmail.com>
2023-03-30 19:29:50 +0100

    add: handle github api response error

--- repo-lang.sh
+++ repo-lang.sh
@@ -12,4 +12,9 @@ echo
 
 url="https://api.github.com/users/${gh_username}/repos"
 
-curl -s "${url}" | jq -r '.[] | "\(.name): \(.language)"'
+if output=$(curl -s $url | jq -r '.[] | "\(.name): \(.language)"' 2> /dev/null); then
+  echo "$output"
+else
+  echo "An error occurred while processing the request."
+  echo "Are you sure the username '${gh_username}' exists?"
+fi
